cfcb69fb8feeeba2c197d6f3ce5960e8058f4d59
bigstone1990 <masatakalman@gmail.com>
2025-01-25 12:16:30 +0900

    ユーザー登録メールテンプレートファイル APP_NAME へ

--- resources/views/emails/user-created.blade.php
+++ resources/views/emails/user-created.blade.php
@@ -1,6 +1,6 @@
 <p class="mb-4">{{$user->name}} 様</p>
 
-<p class="mb-4">Grouperのユーザー登録が完了しました。</p>
+<p class="mb-4">{{ config('app.name', 'Laravel') }}のユーザー登録が完了しました。</p>
 
 <div class="mb-4">
   URL: <a href="https://grouper.fun">https://grouper.fun</a>
